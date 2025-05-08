bdb329f886dc7223421f869867987c6b9f53136c
ValeriaCA9 <vale38646@gmail.com>
2025-05-08 14:21:03 -0600

    Update ejercicio2.c

--- ejercicio2.c
+++ ejercicio2.c
@@ -1,1 +1,20 @@
 //UTILIZANDO UNA LISTA CAPTURAR LAS CINCO CALIFICACIONES DE UN ALUMNO E IMPRIMIR EL PROMEDIO 
+
+#include <stdio.h>
+void main() {
+  int list[5];
+  int i;
+  int suma = 0;
+  int calificacion;
+ 
+
+  printf("Los numeros de la lista son: ");
+
+  for (i = 0; i < 5; i++);
+  {
+    printf("%d", list[i]);
+  }
+    printf("\n");
+  }
+
+NO ES CIERTO CORRIGELO 
